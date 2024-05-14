285807cb4487e6488354cdd0ee4f112c6c50e224
Payton Schlewitt <jc955798@ad.jcu.edu.au>
2024-05-15 09:30:00 +1000

    committing new project to github

---
+++ code/Workshop 2 MOD4.R
@@ -0,0 +1,12 @@
+# Module 4, Workshop 2 - "ggplot2 for communication"
+# author: Payton Schlewitt
+# date: 5/15/2024
+
+
+#install.packages("usethis") ##nly need to run this line once
+#credentials::git_credential_ask()
+#usethis::git_sitrep()
+## Check Personal Access Token
+
+#install.packages("tidyverse") ## ONly need to run this line once
+library("tidyverse") #run at the start to make sure correct package is loaded in
