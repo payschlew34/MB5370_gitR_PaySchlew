baddc3e3192fffe83467f5c021ec67671b06e53f
Payton Schlewitt <jc955798@ad.jcu.edu.au>
2024-05-15 09:44:14 +1000

    saving

--- code/Workshop 2 MOD4.R
+++ code/Workshop 2 MOD4.R
@@ -10,3 +10,27 @@
 
 #install.packages("tidyverse") ## ONly need to run this line once
 library("tidyverse") #run at the start to make sure correct package is loaded in
+
+# Load the data (mpg is built into ggplot2)
+data(mpg)
+
+#Checking data
+head(mpg)
+glimpse(mpg)
+summary(mpg)
+
+#Creating title
+ggplot(mpg, aes(displ, hwy))+
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  labs(title = "Fuel efficiency generally decreases with engine size")
+#Can add subtitles and captions
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  labs(itle = "Fuel efficiency generally decreases with engine size",
+subtitle = "Two seaters (sports cars) are an exception because of their light weight",
+caption = "Data from fueleconomy.gov"
+)
+             
+  
